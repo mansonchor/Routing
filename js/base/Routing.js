d972b36e4784fd830bcef0ab8021f84fccf40aed
mansonchor.github.com <mansonchor@126.com>
2014-05-08 11:05:50 +0800

    add route finish

--- js/base/Routing.js
+++ js/base/Routing.js
@@ -11,20 +11,8 @@ define("base/Routing",[],function(require, exports)
 			var options = options || {}
 			var routes = options.routes || {}
 			
-			var key_arr = __keys(routes)
-			
-			for(var i = 0 ; i< key_arr.length ; i++)
-			{
-				//alert(key_arr[i])
-				__replace_route_to_route_reg(key_arr[i])
-			}
-			
-
-
-			//var arr = "last/8155166/from_profile".match(new_re)
-			//console.log(arr)
-
-			//_routes_config_obj = routes
+			//添加初始化路由
+			__add_routes(routes)
 		}
 
 		Routing.route_start = function(routes)
@@ -33,9 +21,14 @@ define("base/Routing",[],function(require, exports)
 		}
 		
 
+		//把路由匹配规则先转换成正则字符串，方便之后作路由匹配
 		function __replace_route_to_route_reg(route)
 		{
-			var re = /:\w+\/?/g
+			var re = /:\w+\/?/g		//传参规则
+
+			var params_count = route.split(':').length - 1		//参数个数
+
+
 			var reg_str = route.replace(re, function($0)
 			{
 				var replace_role_str = '(\\w+)'
@@ -51,9 +44,7 @@ define("base/Routing",[],function(require, exports)
 			reg_str = reg_str.replace(re,'\\/')
 			reg_str += "$"
 			
-			console.log(reg_str)
-			var new_re = new RegExp(reg_str)
-			console.log(new_re)
+			return { route_reg : reg_str , route : route , params_count : params_count }
 		}
 		
 
@@ -75,9 +66,36 @@ define("base/Routing",[],function(require, exports)
 			return hasOwnProperty.call(obj, key)
 		}
 
-		function __add_route(route , callback)
+		function __is_array(obj) 
 		{
-			_routes_config_obj[route] = callback
+			return toString.call(obj) == '[object Array]'
+		}
+
+		function __add_routes(routes , callback)
+		{
+			var key_arr = __keys(routes)
+
+			//console.log()
+			//数组才循环添加
+			if( __is_array(key_arr) )
+			{
+				for(var i = 0 ; i< key_arr.length ; i++)
+				{
+					var route = key_arr[i]
+					
+					console.log(route)
+					
+					var route_data = __replace_route_to_route_reg(route)
+
+					
+					_routes_config_obj[route] = route_data
+					_routes_config_obj[route].callback = routes[route]
+				}
+			}
+
+			console.log(_routes_config_obj)
+
+			//_routes_config_obj[route] = callback
 		}
 		
 	})(Routing)
